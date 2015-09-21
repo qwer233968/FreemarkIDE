b7bc289a5df61393982e6f782de5af086f20e035
qwer233968 <maqi233968@163.com>
2015-09-21 18:33:56 +0800

    修改insert模板

--- src/main/java/org/qxp/ctrl/mybatis/InsertDirective.java
+++ src/main/java/org/qxp/ctrl/mybatis/InsertDirective.java
@@ -1,7 +1,13 @@
 package org.qxp.ctrl.mybatis;
 
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.qxp.ctrl.mybatis.po.Insert;
+import org.qxp.ctrl.util.Log;
 
 import freemarker.core.Environment;
 import freemarker.template.ObjectWrapper;
@@ -12,10 +18,8 @@ import freemarker.template.TemplateModel;
 import freemarker.template.TemplateModelException;
 
 public class InsertDirective implements TemplateDirectiveModel{
-
-	private static final String ID = "findUser";
-	private static final String RESULT_TYPE = "java.lang.Integer";
-	private static final String SQL = "select * from tb_user";
+	
+	private List<Insert> list;
 	
 	@SuppressWarnings({ "rawtypes", "deprecation" })
 	public void execute(Environment env, Map params, TemplateModel[] loopVars, TemplateDirectiveBody body)
@@ -23,11 +27,33 @@ public class InsertDirective implements TemplateDirectiveModel{
 		if(null == body){
 			throw new TemplateModelException("body null");
 		}else{
-			env.setVariable("id", ObjectWrapper.DEFAULT_WRAPPER.wrap(ID));
-			env.setVariable("resultType", ObjectWrapper.DEFAULT_WRAPPER.wrap(RESULT_TYPE));
-			env.setVariable("sql", ObjectWrapper.DEFAULT_WRAPPER.wrap(SQL));
+			list = formatInsertModel(list);
+			env.setVariable("insertList", ObjectWrapper.DEFAULT_WRAPPER.wrap(list));
 			body.render(env.getOut());
 		}
 	}
+	
+	private List<Insert> formatInsertModel(List<Insert> list){
+		 Iterator<Insert> iter = list.iterator();  
+		 while(iter.hasNext()){  
+			 Insert i = iter.next();
+			 String id = i.getId();
+			 String sql = i.getSql();
+			 if(null == id || null == sql){
+				logger.warn("InsertModel Must include id,sql");
+				 iter.remove();  
+			 }
+		 }
+		 return list;
+	} 
 
+	public List<Insert> getList() {
+		return list;
+	}
+
+	public void setList(List<Insert> list) {
+		this.list = list;
+	}
+
+	private static final Logger logger= Log.getLogger(InsertDirective.class);
 }
